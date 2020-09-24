c59c13d296c46a8e422413b60bd2e02d74855534
Cole Cezeaux <colecezeaux@gmail.com>
2020-09-24 18:15:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function destructivelyRemoveFirstKitten() {
   return kittens
 }
 
-function appendKitten(Broom) {
+function appendKitten(array) {
   array = [...kittens, "Broom"]
   return array
 }
